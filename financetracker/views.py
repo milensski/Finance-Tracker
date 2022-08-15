bad78257c8aad43503c67222d39b076e7ccdcf9b
Milen <feniks9511@gmail.com>
2022-08-15 15:46:10 +0300

    added feature

--- financetracker/views.py
+++ financetracker/views.py
@@ -1,12 +1,10 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from flask_login import  login_required, current_user
-from .models import Expense
+from flask_login import login_required, current_user
+
 from . import db
-import json
+from .models import Expense
 
 views = Blueprint('views', __name__)
-
-
 
 
 @views.route('/home', methods=['GET', 'POST'])
@@ -16,30 +14,31 @@ def home():
         expense_name = request.form.get('expense_name')
         expense_amount = request.form.get('expense_amount')
 
-
-        if not expense_amount.isnumeric():
-            flash('Please enter a number!',category='error')
+        if type(float(expense_amount)) != float and type(int(expense_amount)) != int:
+            flash('Please enter a number!', category='error')
         elif len(expense_name) < 1:
-            flash('Expense is too short',category='error')
+            flash('Expense is too short', category='error')
         elif len(expense_amount) < 1:
-            flash('Expense is too short',category='error')
-        elif int(expense_amount) <= 0:
+            flash('Expense is too short', category='error')
+        elif float(expense_amount) <= 0:
             flash('Expense is can\'t be less than or equal to 0', category='error')
         else:
-            new_expense = Expense(expense_name=expense_name,expense_amount=expense_amount,user_id = current_user.id )
+            new_expense = Expense(expense_name=expense_name, expense_amount=expense_amount, user_id=current_user.id)
             db.session.add(new_expense)
             db.session.commit()
             flash('Expense added', category='success')
 
+    query = Expense.query.all()
+    total = 0
+    for expense in query:
+        if expense.user_id == current_user.id:
+            total += float(expense.expense_amount)
 
-
-
-    return render_template('home.html',user=current_user)
+    return render_template('home.html', user=current_user, total=total)
 
 
 @views.route('/delete/<int:id>', methods=['GET', 'POST'])
 def delete(id):
-
     expense = Expense.query.get(id)
     db.session.delete(expense)
     db.session.commit()
