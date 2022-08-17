03b41122d95d8e2856087fdb0599db3ec755c739
Milen <feniks9511@gmail.com>
2022-08-17 13:29:36 +0300

    corrected code

--- financetracker/__init__.py
+++ financetracker/__init__.py
@@ -1,12 +1,9 @@
-import os
-
-import views as views
-from flask import Flask, redirect, url_for, request, flash, render_template
-from flask_sqlalchemy import SQLAlchemy
 from os import path
-from flask_login import LoginManager, login_required, current_user
+
+from flask import Flask, redirect, url_for
+from flask_login import LoginManager
 from flask_migrate import Migrate
-from werkzeug.utils import secure_filename
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 DB_NAME = 'database.db'
@@ -19,17 +16,18 @@ ALLOWED_EXTENSIONS = {'xls'}
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'zHyouxw7ztlfgkUV6RfLYY4pifWvT8CD'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config[
+        'SQLALCHEMY_DATABASE_URI'] = f'postgres://yudgmktqwiaajb:c1e90bf8c0ee8a70f267a72ac38b4dc4db6a02aa852a39da17ba6703ff5a3563@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/d9n7cr19iafg1t'
 
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-    # app.config['SQLALCHEMY_DATABASE_URI'] = f'postgres://yudgmktqwiaajb:c1e90bf8c0ee8a70f267a72ac38b4dc4db6a02aa852a39da17ba6703ff5a3563@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/d9n7cr19iafg1t'
     @app.route('/')
     def home():
         return redirect(url_for('views.home'))
 
     db.init_app(app)
-    migrate.init_app(app,db)
+    migrate.init_app(app, db)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
@@ -49,7 +47,6 @@ def create_app():
 
     create_database(app)
 
-
     return app
 
 
@@ -57,4 +54,3 @@ def create_database(app):
     if not path.exists('financetracker/' + DB_NAME):
         db.create_all(app=app)
         print('Created Database!')
-
